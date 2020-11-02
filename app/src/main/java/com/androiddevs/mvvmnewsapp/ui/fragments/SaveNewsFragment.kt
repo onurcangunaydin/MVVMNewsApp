be9847f3253f04db0bf118552f74100c06b00eda
Onurcan <onurcangunaydin@gmail.com>
2020-11-02 15:39:20 +0300

    create navigation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/SaveNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class SaveNewsFragment : Fragment(R.layout.fragment_saved_news) {
+}
