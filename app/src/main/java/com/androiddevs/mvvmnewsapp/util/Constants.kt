5f48328054df278671aaa96e3e882021429ac4cd
Onurcan <onurcangunaydin@gmail.com>
2020-11-03 16:37:55 +0300

    Searching News

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "71cc7e6eaa284532852fc6e48c752d45"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
