1994e2ebfc6e299e8e378f63fa2e5ea6bffa3ad5
Onurcan <onurcangunaydin@gmail.com>
2020-11-03 15:47:23 +0300

    Handling Breaking News Response

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
@@ -7,10 +7,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.androiddevs.mvvmnewsapp.models.Article
 
-@Database(
-    entities = [Article::class],
-    version = 1
-)
+@Database(entities = [Article::class], version = 1)
 
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase() {
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModelProviderFactory.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 
-class NewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+class NewsViewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return NewsViewModel(newsRepository) as T
     }
