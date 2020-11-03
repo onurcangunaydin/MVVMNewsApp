d58bd4610e1460d303e947fb69e88b70d35b4a4a
Onurcan <onurcangunaydin@gmail.com>
2020-11-03 18:00:41 +0300

    Showing articles in webview

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -2,8 +2,12 @@ package com.androiddevs.mvvmnewsapp.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
-@Entity(tableName = "articles")
+@Entity(
+
+    tableName = "articles"
+)
 data class Article(
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
@@ -15,4 +19,4 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-)
+): Serializable
